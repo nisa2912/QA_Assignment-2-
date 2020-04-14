7c5fb10ff76b1728910cfac94b75dc1a0291fd06
Noor E Nisa <nisa2912@gmail.com>
2020-04-15 02:41:50 +0600

    test

--- test.py
+++ test.py
@@ -10,6 +10,26 @@ class testApp(unittest.TestCase):
 		statuscode=response.status_code
 		self.assertEqual(statuscode, 200)
 
+	def test_update_product(self):
+
+		result=app.test_client(self)
+		response=result.get('/product')
+		statuscode=response.status_code
+		self.assertEqual(statuscode, 200)
+
+	def test_add_product(self):
+
+		result=app.test_client(self)
+		response=result.get('/product')
+		statuscode=response.status_code
+		self.assertEqual(statuscode, 200)
+
+
+
+
+
+
+
 
 if __name__=="__main__":
 	unittest.main()
